f20298039abdfdea3791d528b3d013655b32b75e
Alirez-13 <alirezaholghi@gmail.com>
2024-10-09 15:10:33 -0700

    Manager controller

--- src/main/java/com/example/demo/controllers/ManagerController.java
+++ src/main/java/com/example/demo/controllers/ManagerController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import com.example.demo.dtos.OrderDto;
 import com.example.demo.dtos.RoomDto;
 import com.example.demo.models.Room;
 import com.example.demo.services.ManagerService;
@@ -16,4 +17,19 @@ public class ManagerController {
     @Autowired
     ManagerService managerService;
 
+
+    @GetMapping
+    public ResponseEntity<List<OrderDto>> getAllRooms() {
+        return new ResponseEntity<>(managerService.getOrderLists(), HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}/accpet")
+    public ResponseEntity<OrderDto> acceptOrderRequest(@PathVariable long id) {
+        return new ResponseEntity<>(managerService.acceptOrder(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}/decline")
+    public ResponseEntity<OrderDto> declineOrderRequest(@PathVariable long id) {
+        return new ResponseEntity<>(managerService.declienOrder(id), HttpStatus.OK);
+    }
 }
