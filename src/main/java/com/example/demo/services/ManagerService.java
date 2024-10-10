3617e7e4355085ca4dd89d0d6e11175217a483ae
Alirez-13 <alirezaholghi@gmail.com>
2024-10-09 18:59:53 -0700

    manager service implementation

--- src/main/java/com/example/demo/services/ManagerService.java
+++ src/main/java/com/example/demo/services/ManagerService.java
@@ -1,32 +1,59 @@
 package com.example.demo.services;
 
 import com.example.demo.dtos.OrderDto;
+import com.example.demo.exception.NotFoundException;
+import com.example.demo.models.Client;
 import com.example.demo.models.Order;
+import com.example.demo.models.Room;
+import com.example.demo.models.enums.OrderStatus;
+import com.example.demo.repositories.IClientRepository;
+import com.example.demo.repositories.IOrderRepository;
+import com.example.demo.repositories.IRoomRepository;
 import com.example.demo.services.interfaces.IManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ManagerService implements IManagerService {
 
 
     @Autowired
-    OrderService orderService;
+    IOrderRepository orderRepository;
+
+    @Autowired
+    IClientRepository clientRepository;
+    @Autowired
+    IRoomRepository roomRepository;
 
     @Override
     public List<OrderDto> getOrderLists() {
-        return orderService.getAllOrders();
+        return orderRepository.findAll().stream().map(x -> toDto(x)).collect(Collectors.toList());
     }
 
     @Override
     public OrderDto acceptOrder(long orderId) {
-        return orderService.acceptOrder(orderId);
+        Order order = orderRepository.findById(orderId).orElseThrow(() -> new NotFoundException("Order not found"));
+        order.setStatus(OrderStatus.Accepted);
+        orderRepository.save(order);
+        return toDto(order);
     }
 
     @Override
     public OrderDto declienOrder(long orderId) {
-        return orderService.rejectOrder(orderId);
+        Order order = orderRepository.findById(orderId).orElseThrow(() -> new NotFoundException("Order not found"));
+        order.setStatus(OrderStatus.Rejected);
+        orderRepository.save(order);
+        return toDto(order);
+    }
+
+    public OrderDto toDto(Order order) {
+        OrderDto orderDto = new OrderDto();
+        orderDto.setRoomNumber(order.getRoom().getRoomNumber());
+        orderDto.setUsername(order.getClient().getUserName());
+        orderDto.setCreatedAt(order.getCreatedAt());
+        return orderDto;
     }
 }
